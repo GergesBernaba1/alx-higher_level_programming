c93e270a2a46ea42cf67cd671ee7f787a02192be
GergesBernaba1 <gergesbernaba2@gmail.com>
2023-11-05 00:00:12 -0700

    taske 7 modfiy

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
-def add_tuple(tuple_a=(), tuple_b=()):
-    # Ensure that the tuples have at least two elements, and if not, use 0 as the default value.
+def add_tuple(tuple_a=(), tuple_b=())
     a = tuple_a + (0, 0)
     b = tuple_b + (0, 0)
-    
-    # Add the elements of the tuples and create a new tuple with the results.
     result_tuple = (a[0] + b[0], a[1] + b[1])
-    
     return result_tuple
