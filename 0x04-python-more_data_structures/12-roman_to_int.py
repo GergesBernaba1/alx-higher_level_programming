62b5829f69725bf62eedf8f7bcb4b5e18f441cac
GergesBernaba1 <gergesbernaba2@gmail.com>
2023-11-09 02:22:12 -0800

    solve task 12. Roman to Integer

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if not roman_string or type(roman_string) is not str:
+    if not roman_string or not isinstance(roman_string, str):
         return 0
-    roman_dict = {
+    roman_numerals = {
         'I': 1,
         'V': 5,
         'X': 10,
@@ -11,16 +11,16 @@ def roman_to_int(roman_string):
         'D': 500,
         'M': 1000
     }
-    result = 0
+    total = 0
     prev_value = 0
-    for roman_char in roman_string[::-1]:
-        value = roman_dict[roman_char]
+    for numeral in reversed(roman_string):
+        value = roman_numerals.get(numeral, 0)
         if value < prev_value:
-            result -= value
+            total -= value
         else:
-            result += value
+            total += value
         prev_value = value
-    return result
+    return total
 if __name__ == "__main__":
     roman_number = "X"
     print("{} = {}".format(roman_number, roman_to_int(roman_number)))
