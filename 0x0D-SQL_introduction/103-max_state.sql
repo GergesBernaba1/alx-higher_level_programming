91d510c7f21ce86c4a49abacc275432ec153aa54
GergesBernaba1 <gergesbernaba2@gmail.com>
2024-01-21 23:19:46 -0800

    correct wrong answers

--- 0x0D-SQL_introduction/103-max_state.sql
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -1,5 +1,5 @@
 -- Display the max temperature of each state (ordered by State name)
-SELECT State, MAX(Temperature) AS max_temp
+SELECT state, MAX(value) AS max_temp
 FROM temperatures
-GROUP BY State
-ORDER BY State;
+GROUP BY state
+ORDER BY state;
--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,2 +1,3 @@
--- Insert a new row into first_table
-INSERT INTO first_table (id, name) VALUES (89, 'Best School');
+-- Insert a new row into first_table, update if duplicate
+INSERT INTO first_table (id, name) VALUES (89, 'Best School')
+ON DUPLICATE KEY UPDATE name = VALUES(name);
