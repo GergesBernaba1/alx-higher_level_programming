5feb396d6e2f0dfe3c3a3c7119f6788ae89596c2
GergesBernaba1 <gergesbernaba2@gmail.com>
2024-02-22 14:30:47 -0800

    solve task

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""
+lists all State objects that contain the letter a from the database
+"""
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sql
